a5dd29f330d62eab7aefe95d9146599cbc4fef92
nshCore <nshcore@protonmail.com>
2019-11-09 21:34:55 +0000

    Add electron react (#3)

---
+++ src/app.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Tabs, Icon, Layout } from 'antd';
+const { Header, Footer, Content } = Layout;
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <Layout>
+                <Header className="Header">
+                    <h3>Aries Wallet</h3>
+                </Header>
+                <Content>
+                    <div className="App">
+                        <Tabs defaultActiveKey="1" style={{ padding: '16px' }}>
+                            <Tabs.TabPane tab={<span><Icon type="wallet" />Wallet</span>} key="1">
+                                Wallet Tab
+                            </Tabs.TabPane>
+                            <Tabs.TabPane tab={<span><Icon type="credit-card" />Payments</span>} key="2">
+                                Payment Tab
+                            </Tabs.TabPane>
+                        </Tabs>
+                    </div>
+                </Content>
+                <Footer>
+                    AriesWallet.io
+                </Footer>
+            </Layout>
+        );
+    }
+}
+
+export default App;
